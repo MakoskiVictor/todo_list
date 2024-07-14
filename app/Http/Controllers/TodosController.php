eec04935268300193c21e6fb35f53647f88f79f3
Victor <vmakoski.training@innevo.cl>
2024-07-14 17:48:58 -0300

    add todos controller

---
+++ app/Http/Controllers/TodosController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Todo;
+
+class TodosController extends Controller
+{
+    /*
+        Convenciones de Laravel:
+
+        index para mostrar todos los ToDos
+        store para guardar un ToDo
+        update para actualizar
+        destroy para eliminar
+        edit para mostrar el formulario de edición
+    */
+    public function store(Request $request){
+        // Validaciones:
+        $request->validate([
+            'title' => 'required|min:3',
+        ]);
+
+        // Construyo la query sql
+        $title = $request->input('title');
+        // $createdAt = now();
+        // $updatedAt = now();
+        // $sql = "INSERT INTO todos (title, createdAt, updatedAt) VALUES (?, ?, ?)";
+        $sql = "INSERT INTO todos (title) VALUES (?)";
+
+        // Ejecutar la consulta sql
+        DB::insert($sql, [$title]);
+
+        // Redirigir una vez concluido el trámite con éxito
+        return redirect()->route('home')->with('success', '¡Tarea creada exitosamente!');
+    }
+}
