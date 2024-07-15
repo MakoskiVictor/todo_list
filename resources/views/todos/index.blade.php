78662b3fed838ddf1375784a7e85b91f49b2ae1f
Victor <vmakoski.training@innevo.cl>
2024-07-15 18:33:16 -0300

    add if else and alerts

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -6,6 +6,13 @@
         <form action="{{ route('agregar') }}" method="POST">
             {{-- Usamos csrf para evitar que el usuario pueda forzar acciones no permitidas (evita que una pagina desconocida pueda hacer cambios) --}}
             @csrf
+            {{-- Uso de if else con Blaze --}}
+            @if (@session('success'))
+                <h6 class=" alert alert-seccess " > {{ session('success') }} </h6>   
+            @endif
+            @error('title')
+            <h6 class=" alert alert-danger " > {{ $message }} </h6> 
+            @enderror
             <div class="mb-3">
             <label for="title" class="form-label">Ingrese Tarea</label>
             <input type="text" name="title" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
